9409d63a44c1d6d8537f8c9b56e6e333e9e737ba
kennone89-a11y <kennone89@gmail.com>
2025-10-31 18:50:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
-// *** VIKTIGT: vi servar MAPPEN "public" ***
-const PUBLIC_DIR = path.join(__dirname, "public");
+const PUBLIC_DIR = path.join(__dirname, "public"); // <- serva "public"
 app.use(express.static(PUBLIC_DIR, { maxAge: 0 }));
 
-// Fallback till index.html (så alltid rätt fil)
 app.get("*", (_, res) => {
   res.sendFile(path.join(PUBLIC_DIR, "index.html"));
 });
